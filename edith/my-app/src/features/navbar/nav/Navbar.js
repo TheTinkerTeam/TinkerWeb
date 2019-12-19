cd1cae938ac4798e2b1d6edd2639e632726f49e3
Edith Santacana <edith.santacana@gmail.com>
2019-12-18 19:05:55 -0800

    Add navbar component

---
+++ edith/my-app/src/features/navbar/nav/Navbar.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import { Menu, Input } from "semantic-ui-react";
+import logo from "../img/SHlogo.png";
+
+class NavBar extends Component {
+  state = { activeItem: "home" };
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+  render() {
+    const { activeItem } = this.state;
+
+    return (
+      <div>
+        <Menu pointing secondary>
+          <Menu.Item>
+            <a>
+              <img src={logo} alt='SHlogo' id='navlogo' />
+            </a>
+          </Menu.Item>
+		  <Menu.Item
+            name='Home'
+            active={activeItem === "Home"}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name='Projects'
+            active={activeItem === "Projects"}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name='Tutorials'
+            active={activeItem === "Tutorials"}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name='TinkerCart'
+            active={activeItem === "TinkerCart"}
+            onClick={this.handleItemClick}
+          />
+		  <Menu.Item
+            name='Membership'
+            active={activeItem === "Membership"}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item>
+            <Input className='icon' icon='search' placeholder='Search...' id="search-menubar" />
+          </Menu.Item>
+          <Menu.Menu position='right'>
+            <Menu.Item
+              name='Join Supertinker'
+              active={activeItem === "Join Supertinker"}
+              onClick={this.handleItemClick}
+            />
+            <Menu.Item
+              name='Sign in'
+              active={activeItem === "Sign in"}
+              onClick={this.handleItemClick}
+            />
+          </Menu.Menu>
+        </Menu>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
