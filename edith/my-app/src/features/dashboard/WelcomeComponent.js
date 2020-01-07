7295a002cdd45432ca5177e07f7e653df9c8ff23
Edith Santacana <edith.santacana@gmail.com>
2020-01-06 18:33:03 -0800

    Add css grid minmax row height

--- edith/my-app/src/features/dashboard/WelcomeComponent.js
+++ edith/my-app/src/features/dashboard/WelcomeComponent.js
@@ -1,16 +1,25 @@
 import React, { Component } from "react";
 import WelcomeList from "./WelcomeList";
+import tinkercart from "./img/tinkercart.png";
 
 class WelcomeComponent extends Component {
   state = {
-    items: ["Try out starter projects", "Join a class with your class code", "Download the Tinker Cart plans", "wow"]
+    items: [
+      "Try out starter projects",
+      "Join a class with your class code",
+      "Download the Tinker Cart plans",
+      "wow"
+    ]
   };
 
   render() {
     return (
       <div>
         <div className='red-title'>Welcome to SuperTinker</div>
-        <WelcomeList items={this.state.items}/>
+        <a>
+          <img src={tinkercart} alt='Tinker Cart model' height='42' width='42' />
+        </a>
+        <WelcomeList items={this.state.items} />
       </div>
     );
   }
