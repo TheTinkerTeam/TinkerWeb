9e829c19d1032628ef8dccf052552ac0df629a74
Edith Santacana <edith.santacana@gmail.com>
2020-01-13 16:21:12 -0800

    update SignIn & JoinSUperTinker buttons css (in the navbar)

--- edith/my-app/src/features/navbar/menus/SignedOutMenu.js
+++ edith/my-app/src/features/navbar/menus/SignedOutMenu.js
@@ -1,27 +1,33 @@
-import React from 'react'
+import React from "react";
 import { Menu, Button } from "semantic-ui-react";
+import './SignedOutMenu.css';
 
-const SignedOutMenu = ({signIn}) => {
-	return (
-		<Menu.Item position='right'>
-            <Button
-			onClick={signIn}
-              content='Join SuperTinker'
-              basic
-            />
-            <Button
-			onClick={signIn}
-              content='Sign in'
-			  basic
-			  style={{ marginLeft: '0.5em'}}
-            />
-          </Menu.Item>
-	)
-}
+const SignedOutMenu = ({ signIn }) => {
+  return (
+    <Menu.Item position='right'>
+      <Button
+        onClick={signIn}
+        content='Join SuperTinker'
+        basic
+        id='joinSuperTinkerButton'
+		className='custom'
+      />
+      <Button
+        onClick={signIn}
+        content='Sign in'
+        basic
+        style={{ marginLeft: "0.5em" }}
+        id='signInButton'
+		className='custom'
+      />
+    </Menu.Item>
+  );
+};
 
 export default SignedOutMenu;
 
-{/* <Menu.Item
+{
+  /* <Menu.Item
 	name='Join Supertinker'
 	active={activeItem === "Join Supertinker"}
 	onClick={this.handleItemClick}
@@ -30,4 +36,5 @@ export default SignedOutMenu;
 	name='Sign in'
 	active={activeItem === "Sign in"}
 	onClick={this.handleItemClick}
-/> */}
+/> */
+}
