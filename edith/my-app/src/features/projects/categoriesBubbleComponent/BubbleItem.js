8e1a5ee56c592b2b4e5e909f5ef3acdf6059f2e2
Edith Santacana <edith.santacana@gmail.com>
2020-01-15 22:19:03 -0800

    remove comments

--- edith/my-app/src/features/projects/categoriesBubbleComponent/BubbleItem.js
+++ edith/my-app/src/features/projects/categoriesBubbleComponent/BubbleItem.js
@@ -7,21 +7,6 @@ class BubbleItem extends Component {
 	super(props);
   }
 
-//   handleActiveSelection = (newActiveItem) => {
-// 	newActiveItem.name = evt.target.value;
-//     this.setState(({ activeItem }) => ({
-//       activeItem: [...activeItem, newActiveItem]
-//     }));
-//   };
-
-//   handleUnactiveSelection = id => {
-//     this.setState(({ activeItem }) => ({
-//       activeItem: activeItem.filter(
-//         activeItem => activeItem.id !== id //returns the elements of the array that does not match the id that we are passing in our parameter
-//       )
-//     }));
-//   };
-
   render() {
     const { category, isActive, handleSelection, handleUnselection } = this.props;
 	return <Button onClick={() => handleSelection(category)} className={'bubble-container ' + (isActive ? 'active' : '') }>{category.name}</Button>;
