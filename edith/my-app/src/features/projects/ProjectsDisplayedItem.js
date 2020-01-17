6005d7b39ab37d7a9a911ee66c3f954e4f634da7
Edith Santacana <edith.santacana@gmail.com>
2020-01-17 12:48:45 -0800

    update css for displayed projects + add project detailed link to image

--- edith/my-app/src/features/projects/ProjectsDisplayedItem.js
+++ edith/my-app/src/features/projects/ProjectsDisplayedItem.js
@@ -3,7 +3,24 @@ import React, { Component } from "react";
 class ProjectsDisplayedItem extends Component {
   render() {
     const { project } = this.props;
-    return <div>Hello {project.title}</div>;
+    return (
+      <div className='wrapper'>
+        <div className='displayed-projects-item'>
+          <div className='child'>
+            <a href={`/projects/${project.id}`}>
+              <img
+                src={project.imageURL}
+                className=''
+                alt={project.title}
+                height='100em'
+                width='100em'
+              />
+            </a>
+          </div>
+          <div className='child'>{project.title}</div>
+        </div>
+      </div>
+    );
   }
 }
 export default ProjectsDisplayedItem;
--- edith/my-app/src/features/projects/ProjectsDisplayedList.js
+++ edith/my-app/src/features/projects/ProjectsDisplayedList.js
@@ -5,7 +5,7 @@ class ProjectsDisplayedList extends Component {
   render() {
     const { projects } = this.props;
 
-    return <div>
+    return <div className='projects-display-flex-container'>
         {projects.map((project) => (
           <ProjectsDisplayedItem key={project.id} project={project} />
         ))}
