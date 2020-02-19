faa2835b81dd4a08fe85406b469cf4f92078b7a7
Edith Santacana <edith.santacana@gmail.com>
2020-02-19 14:33:48 -0800

    add SigninComponent

---
+++ edith/my-app/src/auth/SigninComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class SigninComponent extends Component {
+	render() {
+		return (
+			<div>
+				This is the SignIn page
+			</div>
+		)
+	}
+}
+
+export default SigninComponent;
--- edith/my-app/src/features/navbar/menus/SignedOutMenu.js
+++ edith/my-app/src/features/navbar/menus/SignedOutMenu.js
@@ -1,28 +1,31 @@
 import React from "react";
 import { Menu, Button, Icon } from "semantic-ui-react";
 import "./SignedOutMenu.css";
+import { Link } from "react-router-dom";
 
 const SignedOutMenu = ({ signIn }) => {
   return (
     <Menu.Item position='right'>
-      <Button
+      {/* <Button
         onClick={signIn}
         content='Sign in'
         basic
         style={{ marginLeft: "0.5em" }}
         className='custom'
-		id='signInButton'
-      />
+        id='signInButton'
+      /> */}
       <Button
-        onClick={signIn}
+        as={Link}
+        to='/signin'
         basic
         className='custom'
-		icon labelPosition='right'
-		id='getStartedButton'
+        icon
+        labelPosition='right'
+        id='getStartedButton'
       >
-	  Get Started
-	  <Icon name='angle right' />
-	  </Button>
+        Get Started
+        <Icon name='angle right' />
+      </Button>
     </Menu.Item>
   );
 };
