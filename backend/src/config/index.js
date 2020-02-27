8fee77617afecaf3c117beef90bfc6700e6aba95
jkaplin <kaplinjoseph@gmail.com>
2020-02-27 09:52:01 -0800

    Change config file

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -1,42 +1,42 @@
 const dev = {
- app: {
-   port: parseInt(process.env.DEV_APP_PORT) || 5000
- },
- db: {
-   host: process.env.DEV_DB_HOST,
-   user: process.env.DB_USER,
-   password: process.env.DB_PASS
- }
+  app: {
+    port: parseInt(process.env.DEV_APP_PORT) || 5000
+  },
+  db: {
+    host: process.env.DEV_DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS
+  }
 };
 
 const prod = {
- app: {
-   port: parseInt(process.env.TEST_APP_PORT) || 5000
- },
- db: {
-   host: process.env.PROD_DB_HOST,
-   user: process.env.DB_USER,
-   password: process.env.DB_PASS
- }
+  app: {
+    port: parseInt(process.env.TEST_APP_PORT) || 5000
+  },
+  db: {
+    host: process.env.PROD_DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS
+  }
 };
 
 const test = {
- app: {
-   port: parseInt(process.env.TEST_APP_PORT) || 5000
- },
- db: {
-   host: process.env.TEST_DB_HOST,
-   user: process.env.DB_USER,
-   password: process.env.DB_PASS
- }
+  app: {
+    port: parseInt(process.env.TEST_APP_PORT) || 5000
+  },
+  db: {
+    host: process.env.TEST_DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS
+  }
 };
 
 const config = {
- dev,
- prod,
- test
+  dev,
+  prod,
+  test
 };
 
-const env = process.env.NODE_ENV; // 'dev', 'prod' or 'test'
+const env = "dev"; // 'dev', 'prod' or 'test'
 
 module.exports = config[env];
