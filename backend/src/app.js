cb6d27d664b2d40d54ded3c389fd5379ef183b97
jkaplin <kaplinjoseph@gmail.com>
2020-02-28 08:45:26 -0800

    FIX

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,12 +19,12 @@ app.use("/api/v1/profiles", profileRoutes);
 app.use("/api/v1/projects", projectRoutes);
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname + "../../frontend/build")));
+app.use(express.static(path.join(__dirname, "../../frontend/build")));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "../../frontend/build/index.html"));
+  res.sendFile(path.join(__dirname, "../../frontend/build/index.html"));
 });
 
 module.exports = app;
