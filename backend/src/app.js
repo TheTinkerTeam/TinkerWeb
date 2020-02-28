717ac81a15544127a720aa4458c587fea3d8cf7d
jkaplin <kaplinjoseph@gmail.com>
2020-02-28 08:58:28 -0800

    Fix routing in branch

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,7 +23,7 @@ app.use("/api/v1/projects", projectRoutes);
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "../../frontend/build/index.html"));
+  res.sendFile(path.join(__dirname, "../../frontend/build/index.html"));
 });
 
 module.exports = app;
