34968369f41f6cb2fac239ea5ebc4cd7af6b8c3f
Edith Santacana <edith.santacana@gmail.com>
2020-03-04 09:58:33 -0800

    update NavBar > responsive

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Menu, Input } from "semantic-ui-react";
+import { Menu, Input, Responsive, Button } from "semantic-ui-react";
 import { NavLink, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 
@@ -10,28 +10,75 @@ import SignedInMenu from "../navbar/SignedInMenu";
 const NavBar = ({ auth }) => {
   return (
     <div>
-      <Menu pointing secondary fixed="top">
-        {/* <Menu.Item>
-            <a href='/'>
-              <img src={logo} alt='SHlogo' id='navlogo' />
-            </a>
-          </Menu.Item> */}
-        <Menu.Item as={NavLink} exact to="/" name="Home">
-          <img src={logo} alt="SHlogo" id="navlogo" />
-        </Menu.Item>
-        <Menu.Item as={NavLink} to="/projects" name="Projects" />
-        <Menu.Item as={NavLink} to="/tutorials" name="Tutorials" />
-        <Menu.Item as={NavLink} to="/tinkercart" name="TinkerCart" />
-        <Menu.Item as={NavLink} to="/membership" name="Membership" />
-        <Menu.Item as={NavLink} to="/test" name="Test" />
-        <Menu.Item>
-          <Input
-            className="icon"
-            icon="search"
-            placeholder="Search..."
-            id="search-menubar"
+      <Menu pointing secondary fixed='top'>
+        <Menu.Item as={NavLink} exact to='/' name='Home'>
+          <Responsive
+            {...Responsive.onlyMobile}
+            as={Button}
+            content='dropdown menu(mobile)'
+            icon='list'
+            labelPosition='left'
           />
+          {/* <Responsive
+              as={Button}
+              content='desktop version'
+              minWidth={Responsive.onlyTablet.minWidth}
+            /> */}
+          <Responsive minWidth={Responsive.onlyTablet.minWidth}>
+            <img src={logo} alt='SHlogo' id='navlogo' />
+          </Responsive>
         </Menu.Item>
+        <Responsive minWidth={Responsive.onlyTablet.minWidth}>
+          <Menu.Item
+            style={{ marginTop: "0.6em" }}
+            as={NavLink}
+            to='/projects'
+            name='Projects'
+          />
+        </Responsive>
+        <Responsive minWidth={Responsive.onlyTablet.minWidth}>
+          <Menu.Item
+            style={{ marginTop: "0.6em" }}
+            as={NavLink}
+            to='/tutorials'
+            name='Tutorials'
+          />
+        </Responsive>
+        <Responsive minWidth={Responsive.onlyTablet.minWidth}>
+          <Menu.Item
+            style={{ marginTop: "0.6em", whiteSpace: "nowrap" }}
+            as={NavLink}
+            to='/tinkercart'
+            name='TinkerCart'
+          />
+        </Responsive>
+        <Responsive minWidth={Responsive.onlyTablet.minWidth}>
+          <Menu.Item
+            style={{ marginTop: "0.6em" }}
+            as={NavLink}
+            to='/membership'
+            name='Membership'
+          />
+        </Responsive>
+        <Responsive minWidth={Responsive.onlyTablet.minWidth}>
+          <Menu.Item
+            style={{ marginTop: "0.6em" }}
+            as={NavLink}
+            to='/test'
+            name='Test'
+          />
+        </Responsive>
+        <Responsive minWidth={Responsive.onlyTablet.minWidth && 1100}>
+          <Menu.Item>
+            <Input
+            style={{ marginTop: "0.4em" }}
+              className='icon'
+              icon='search'
+              placeholder='Search...'
+              id='search-menubar'
+            />
+          </Menu.Item>
+        </Responsive>
         {/* <Menu.Menu position='right'>
             <Menu.Item
               name='Join Supertinker'
