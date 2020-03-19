3423959e9c198e018add6ef1d044fff0f3b090c9
Edith Santacana <edith.santacana@gmail.com>
2020-03-18 18:52:05 -0700

    add 'add a new class' button

--- frontend/src/components/pages/classrooms/ClassroomsPage.js
+++ frontend/src/components/pages/classrooms/ClassroomsPage.js
@@ -1,21 +1,30 @@
 import React, { Component } from "react";
-import { Segment, Header, Icon, Button } from "semantic-ui-react";
-import FlexClassroomList from '../../lists/FlexClassroomList'
+import { Segment, Header, Icon, Button, Divider } from "semantic-ui-react";
+import FlexClassroomList from "../../lists/FlexClassroomList";
 
-const square = { width: 175, height: 175};
+const square = { width: 175, height: 175 };
 
 class ClassroomsPage extends Component {
   render() {
     return (
       <div>
-        <div className='project-title-style'>Hi Classroom!</div>
-        <FlexClassroomList/>
-        <Segment circular inverted style={square}>
-        <Header inverted as='h2'>
+        <div className='classrooms-title-container'>
+          <div className='classroom-title-style'>Hi Classroom!</div>
+        </div>
+        <FlexClassroomList />
+        <Divider style={{marginTop: "3em"}}/>
+        {/* <Segment circular inverted style={square}>
+          <Header inverted as='h2'>
             Add a new class
           </Header>
-        <Button circular icon='add' />
-        </Segment>
+          <Button circular icon='add' />
+        </Segment> */}
+        <div>
+          <Button className='transparent-button' icon labelPosition='left'>
+            <Icon name='plus' />
+            Add a new class
+          </Button>
+        </div>
       </div>
     );
   }
