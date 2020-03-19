81cfedd99e109fdcf44200f534266115fc165faf
Edith Santacana <edith.santacana@gmail.com>
2020-03-19 16:59:31 -0700

    add dropdown menu for students

--- frontend/src/components/pages/classrooms/ClassroomDetailsPage.js
+++ frontend/src/components/pages/classrooms/ClassroomDetailsPage.js
@@ -1,14 +1,53 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import "../../../css/Classrooms.css";
+import { Segment, Button } from "semantic-ui-react";
 
 class ClassroomDetailsPage extends Component {
-	render() {
-		return (
-			<div className="classroom-title-style">
-				Hi, I am the ClassroomDetailsPage!
-			</div>
-		)
-	}
+  constructor(props) {
+    super(props);
+    this.state = { isStudentsActive: true };
+  }
+
+  render() {
+    const { isStudentsActive } = this.state;
+
+    const toggleButton = () => {
+      this.setState(({ isStudentsActive }) => ({
+        isStudentsActive: !isStudentsActive
+      }));
+    };
+
+    return (
+      <div>
+        <div className='classroom-title-style'>
+          Hi, I am the "GRADE" ClassroomDetailsPage!
+        </div>
+        <Segment.Group className='paragraph-style display-in-box'>
+          <Segment className='section-title'>
+            <div className='flexbox-container'>
+              <div>STUDENTS</div>
+              {isStudentsActive ? (
+                <Button
+                  className='arrow-down-button-classroom'
+                  circular
+                  icon='angle up'
+                  onClick={toggleButton}
+                />
+              ) : (
+                <Button
+                  className='arrow-down-button-classroom'
+                  circular
+                  icon='angle down'
+                  onClick={toggleButton}
+                />
+              )}
+            </div>
+            {isStudentsActive && <div>Blabla</div>}
+          </Segment>
+        </Segment.Group>
+      </div>
+    );
+  }
 }
 
 export default ClassroomDetailsPage;
