095e216b76ca208415f0a0dd6a5dc18f5128b681
Edith Santacana <edith.santacana@gmail.com>
2020-03-19 10:31:52 -0700

    add assignements in classrooms

--- frontend/src/components/lists/FlexClassroomList.js
+++ frontend/src/components/lists/FlexClassroomList.js
@@ -7,10 +7,27 @@ class FlexClassroomList extends Component {
     super(props);
     this.state = {
       list: [
-        { class: "Grade 5A", nb_students: "12", id: "1" },
-        { class: "Grade 5B", nb_students: "15", id: "2" },
-        { class: "Grade 6A", nb_students: "14", id: "3" },
-        { class: "Grade 6B", nb_students: "11", id: "4" }
+        {
+          class: "Grade 5A",
+          subject: "Design",
+          nb_students: "12",
+          id: "1",
+          assignement: [
+            {date: "03/20/20", task: "Do your homework"},
+            {date: "03/24/20", task: "Finish the plan"}
+          ]
+        },
+        {
+          class: "Grade 5B",
+          subject: "Maths",
+          nb_students: "15",
+          id: "2",
+          assignement: [
+            { date: "03/21/20", task: "List the material you need please" }
+          ]
+        },
+        { class: "Grade 6A", assignement: [], subject: "Interdiscplinary", nb_students: "14", id: "3" },
+        { class: "Grade 6B", subject: "General", nb_students: "11", id: "4" }
       ]
     };
   }
