53acf816c82c64bb0239eb9d00d3c0c40b9086c0
Edith Santacana <edith.santacana@gmail.com>
2020-05-08 12:43:07 -0700

    [students profile] update settings button css

--- frontend/src/components/pages/userProfile/UserProfileHeader.js
+++ frontend/src/components/pages/userProfile/UserProfileHeader.js
@@ -7,44 +7,46 @@ const UserProfileHeader = ({ auth, student }) => {
   return (
     <Grid.Column width={16}>
       <Segment>
-        <Item.Group>
-          <Item>
-            <Item.Image
-              avatar
-              size='small'
-              src={
-                student.avatar
-                  ? student.avatar === "defaultImgUrl"
-                    ? avatar
-                    : student.avatar
-                  : "https://randomuser.me/api/portraits/men/20.jpg"
-              }
-            />
-            <Item.Content verticalAlign='bottom'>
-              <Header as='h1'>
-                {student.firstName} {student.lastName}
-              </Header>
-              <br />
-              {/* <Header as='h3'>{capitalize(`${student.role}`)}</Header> */}
-              {/* <br /> */}
-              <Header as='h2'>@{student.username}</Header>
-            </Item.Content>
-            <Item.Content verticalAlign='bottom'>
-              {student.uid === auth.uid && (
-                <Button
-                  as={Link}
-                  to='/settings'
-                  //color='teal'
-                  //fluid
-                  basic
-                  circular
-                  icon='settings'
-                  style={{padding: "10px"}}
-                />
-              )}
-            </Item.Content>
-          </Item>
-        </Item.Group>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <Item.Group>
+            <Item>
+              <Item.Image
+                avatar
+                size='small'
+                src={
+                  student.avatar
+                    ? student.avatar === "defaultImgUrl"
+                      ? avatar
+                      : student.avatar
+                    : "https://randomuser.me/api/portraits/men/20.jpg"
+                }
+              />
+              <Item.Content verticalAlign='bottom'>
+                <Header as='h1'>
+                  {student.firstName} {student.lastName}
+                </Header>
+                <br />
+                {/* <Header as='h3'>{capitalize(`${student.role}`)}</Header> */}
+                {/* <br /> */}
+                <Header as='h2'>@{student.username}</Header>
+              </Item.Content>
+            </Item>
+          </Item.Group>
+          <div>
+            {student.uid === auth.uid && (
+              <Button
+                as={Link}
+                to='/settings'
+                color='standard'
+                //fluid
+                //basic
+                circular
+                icon='settings'
+                //style={{ marginLeft: "87%", padding: "10px" }}
+              />
+            )}
+          </div>
+        </div>
       </Segment>
     </Grid.Column>
   );
